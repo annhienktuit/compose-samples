aaee6e999e7efa365997b2d76014814e6c89acf9
nhiennha <nhiennha@vng.com.vn>
2024-07-07 16:45:11 +0700

    make image circular

--- Jetcaster/mobile/src/main/java/com/example/jetcaster/ui/profile/ProfileScreen.kt
+++ Jetcaster/mobile/src/main/java/com/example/jetcaster/ui/profile/ProfileScreen.kt
@@ -4,7 +4,9 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -14,8 +16,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.min
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.window.core.layout.WindowSizeClass
 import coil.compose.AsyncImagePainter
@@ -28,38 +32,22 @@ fun ProfileScreen(
     windowSizeClass: WindowSizeClass,
     onBackPress: () -> Unit,
     viewModel: ProfileViewModel = hiltViewModel(),
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier.padding(top = 32.dp)
 ) {
-    val profileInfo = ProfileInfo(
-        "1",
-        "Maguire",
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZdq8BpP8g-6ArJZxW8OqyLzWfeaIJIK-5Lw&s"
+    ProfileHeader(
+        profileInfo = ProfileInfo(
+            "1",
+            "Maguire",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZdq8BpP8g-6ArJZxW8OqyLzWfeaIJIK-5Lw&s"
+        ),
+        modifier = modifier
     )
-    var imagePainterState by remember {
-        mutableStateOf<AsyncImagePainter.State>(AsyncImagePainter.State.Empty)
-    }
-    val imageLoader = rememberAsyncImagePainter(
-        model = ImageRequest.Builder(LocalContext.current)
-            .data(profileInfo.avatarUrl)
-            .crossfade(true)
-            .build(),
-        contentScale = ContentScale.Crop,
-        onState = { state -> imagePainterState = state }
-    )
-    Column(modifier = modifier) {
-        Image(
-            painter = imageLoader,
-            contentDescription = "Avatar",
-            contentScale = ContentScale.Crop,
-            modifier = modifier.size(168.dp).clip(MaterialTheme.shapes.medium),
-        )
-    }
 }
 
 @Composable
 fun ProfileHeader(
     profileInfo: ProfileInfo,
-    modifier: Modifier = Modifier.padding(16.dp)
+    modifier: Modifier
 ) {
     var imagePainterState by remember {
         mutableStateOf<AsyncImagePainter.State>(AsyncImagePainter.State.Empty)
@@ -77,7 +65,14 @@ fun ProfileHeader(
             painter = imageLoader,
             contentDescription = "Avatar",
             contentScale = ContentScale.Crop,
-            modifier = modifier,
+            modifier = modifier.size(168.dp).clip(CircleShape),
+        )
+        Text(
+            text = profileInfo.name,
+            style = MaterialTheme.typography.labelSmall,
+            modifier = Modifier.padding(top = 8.dp),
+            fontSize = 20.sp,
+            textAlign = TextAlign.Center
         )
     }
 }
